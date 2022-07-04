9e1ef53ad7a43bea62adea9b56893a6777771304
czafersyuz <czafersyuz@gmail.com>
2022-07-04 17:24:59 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.scss';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>welcome react</h1>
+    
+      <Navbar/>
       
     </div>
   );
